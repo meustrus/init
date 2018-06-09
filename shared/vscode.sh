049c641ddc5cd15e5f69c9c3868433fe899ef61c
meustrus <meustrus@gmail.com>
2018-06-09 17:57:18 -0500

    Fix vscode settings symlink

--- shared/vscode.sh
+++ shared/vscode.sh
@@ -3,8 +3,8 @@
 INIT_REPO=${INIT_REPO:-$(cd "$(dirname "$0")/.." && pwd)}
 
 finalizeVSCode() {
-    ln -s -f "$INIT_REPO/shared/vscode/settings.json" "$1/settings.json"
-    ln -s -f "$INIT_REPO/shared/vscode/keybindings.json" "$1/keybindings.json"
+    ln -sf "$INIT_REPO/shared/vscode/settings.json" "$1/settings.json"
+    ln -sf "$INIT_REPO/shared/vscode/keybindings.json" "$1/keybindings.json"
 
     git config --global diff.tool code
     git config --global merge.tool code
@@ -14,7 +14,7 @@ finalizeVSCode() {
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     brew cask list visual-studio-code >/dev/null 2>&1 || brew cask install --appdir="$MAC_APPS" visual-studio-code
-    finalizeVSCode "~/Library/Application Support/Code/User/"
+    finalizeVSCode "$HOME/Library/Application Support/Code/User"
 elif [[ "$OSTYPE" == "msys"* ]]; then
     choco install vscode -y
     finalizeVSCode "$APPDATA/Code/User/"
