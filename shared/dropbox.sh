c987af02214f0f0656cfc5138f546b449420cd11
meustrus <meustrus@gmail.com>
2018-06-13 16:52:53 -0500

    Delete some unused/problematic things

--- shared/dropbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew cask list dropbox >/dev/null 2>&1 || brew cask install --appdir="$MAC_APPS" dropbox
-elif [[ "$OSTYPE" == "msys"* ]]; then
-    choco install dropbox -y
-fi
--- win/ssh.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-choco install openssh -y
-call %~dp0resetenv.bat
