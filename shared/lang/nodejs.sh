0df4e59ac449d7fc38e70c0939589df4ac26280d
meustrus <meustrus@gmail.com>
2018-06-14 12:05:49 -0500

    Fix NodeJS installing Mac-specific things everywhere

--- shared/lang/nodejs.sh
+++ shared/lang/nodejs.sh
@@ -3,22 +3,23 @@
 if [ -x "$(command -v nvm)" ]; then
     if printf '%s' "$OSTYPE" | grep -q darwin 2>/dev/null; then
         brew list nvm >/dev/null 2>&1 || brew install nvm
+
+        mkdir ~/.nvm
+
+        echo >> ~/.bash_profile
+        echo \# NVM >> ~/.bash_profile
+        echo export NVM_DIR=~/.nvm >> ~/.bash_profile
+        echo source \$(brew --prefix nvm)/nvm.sh >> ~/.bash_profile
+
+        echo >> ~/.zprofile
+        echo \# NVM >> ~/.zprofile
+        echo export NVM_DIR=~/.nvm >> ~/.zprofile
+        echo source \$(brew --prefix nvm)/nvm.sh >> ~/.zprofile
+
+        source ~/.bash_profile
     elif printf '%s' "$OSTYPE" | grep -q msys 2>/dev/null; then
         choco install nvm -y
     fi
-    mkdir ~/.nvm
-
-    echo >> ~/.bash_profile
-    echo \# NVM >> ~/.bash_profile
-    echo export NVM_DIR=~/.nvm >> ~/.bash_profile
-    echo source \$(brew --prefix nvm)/nvm.sh >> ~/.bash_profile
-
-    echo >> ~/.zprofile
-    echo \# NVM >> ~/.zprofile
-    echo export NVM_DIR=~/.nvm >> ~/.zprofile
-    echo source \$(brew --prefix nvm)/nvm.sh >> ~/.zprofile
-
-    source ~/.bash_profile
 fi
 
 nvm install node --reinstall-packages-from=node
