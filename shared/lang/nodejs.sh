305e9121a4ac2b7edf46014ad3dd8edac008cb5a
meustrus <meustrus@gmail.com>
2018-06-16 11:35:06 -0500

    Improve NodeJS install for Windows

--- shared/lang/nodejs.sh
+++ shared/lang/nodejs.sh
@@ -3,12 +3,13 @@
 INIT_REPO=${INIT_REPO:-$(cd "$(dirname "$0")/../.." && pwd)}
 source "$INIT_REPO/shared/vars.sh"
 
+export NVM_DIR=${NVM_DIR:-$BENCH/lib/.nvm}
+${SUDO_USER:+sudo -u "$SUDO_USER"} mkdir -p "$NVM_DIR"
+
 if [ -x "$(command -v nvm)" ]; then
     if printf '%s' "$OSTYPE" | grep -q darwin 2>/dev/null; then
         sh "$INIT_REPO/mac/bin/brew-install.sh" nvm
-        
-        export NVM_DIR="$BENCH/lib/.nvm"
-        ${SUDO_USER:+sudo -u "$SUDO_USER"} mkdir -p "$NVM_DIR"
+
         source $(${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} brew --prefix nvm)/nvm.sh
 
         sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.profile"
@@ -16,6 +17,7 @@ if [ -x "$(command -v nvm)" ]; then
         sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.zprofile"
     elif printf '%s' "$OSTYPE" | grep -q msys 2>/dev/null; then
         choco install nvm -y
+        setx NVM_DIR "$NVM_DIR"
     fi
 fi
 
