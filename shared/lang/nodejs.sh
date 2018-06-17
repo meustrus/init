da71f0956f95bf4ffbb2e806e44ba18f769a561c
meustrus <meustrus@gmail.com>
2018-06-17 13:14:14 -0500

    Fix NodeJS installation for Mac

--- shared/lang/nodejs.sh
+++ shared/lang/nodejs.sh
@@ -6,20 +6,19 @@ source "$INIT_REPO/shared/vars.sh"
 export NVM_DIR=${NVM_DIR:-$BENCH/lib/.nvm}
 ${SUDO_USER:+sudo -u "$SUDO_USER"} mkdir -p "$NVM_DIR"
 
-if [ -x "$(command -v nvm)" ]; then
-    if printf '%s' "$OSTYPE" | grep -q darwin 2>/dev/null; then
-        sh "$INIT_REPO/mac/bin/brew-install.sh" nvm
+if printf '%s' "$OSTYPE" | grep -q darwin 2>/dev/null; then
+    sh "$INIT_REPO/mac/bin/brew-install.sh" nvm
 
-        source $(${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} brew --prefix nvm)/nvm.sh
+    source $(${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} brew --prefix nvm)/nvm.sh
 
-        sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.profile"
-        sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.bash_profile"
-        sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.zprofile"
-    elif printf '%s' "$OSTYPE" | grep -q msys 2>/dev/null; then
-        choco install nvm -y
-        setx NVM_DIR "$NVM_DIR"
-    fi
+    sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.profile"
+    sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.bash_profile"
+    sh "$INIT_REPO/shared/bin/append" "# NVM\nexport NVM_DIR=\"$NVM_DIR\"\nsource \$(brew --prefix nvm)/nvm.sh" "$HOME/.zprofile"
+elif printf '%s' "$OSTYPE" | grep -q msys 2>/dev/null; then
+    choco install nvm -y
+    setx NVM_DIR "$NVM_DIR"
 fi
 
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} nvm install node
 ${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} nvm install node --reinstall-packages-from=node
 ${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} nvm use node
