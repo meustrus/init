0eabe0badbaaaa8628347a6d4bb223996ec09ef1
meustrus <meustrus@gmail.com>
2018-08-30 14:54:32 -0500

    fixup making $BENCH

--- shared/bench.sh
+++ shared/bench.sh
@@ -3,11 +3,10 @@
 INIT_REPO=${INIT_REPO:-$(cd "$(dirname "$0")/.." && pwd)}
 source "$INIT_REPO/shared/vars.sh"
 
-mkdir -p "$BENCH/ext"
-mkdir -p "$BENCH/lib"
-mkdir -p "$BENCH/repo"
-mkdir -p "$BENCH/usr"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/bin"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/ext"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/lib"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/repo"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/usr"
 
-sh "$INIT_REPO/shared/bin/append" "export PATH=\"$BENCH:\$PATH\"" "$HOME/.profile"
-sh "$INIT_REPO/shared/bin/append" "export PATH=\"$BENCH:\$PATH\"" "$HOME/.bash_profile"
-sh "$INIT_REPO/shared/bin/append" "export PATH=\"$BENCH:\$PATH\"" "$HOME/.zprofile"
+"$INIT_REPO/shared/bin/install-path" "$BENCH/bin"
