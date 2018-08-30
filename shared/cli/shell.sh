1beefeee11dc7eb145e148f3d99a2b6cece907e8
meustrus <meustrus@gmail.com>
2018-08-30 15:33:39 -0500

    Rename `config` directory to `etc`

--- shared/cli/shell.sh
+++ shared/cli/shell.sh
@@ -3,7 +3,7 @@
 source "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
 ${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} mkdir -p "$BENCH/shellrc.d"
-${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} cp -f "$INIT_REPO/shared/config/initrc.sh" "$BENCH/shellrc.d/initrc.sh"
+${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} cp -f "$INIT_REPO/shared/etc/initrc.sh" "$BENCH/shellrc.d/initrc.sh"
 
 loadrc="for filename in \"$BENCH\"/shellrc.d/*.sh; do source \"\$filename\"; done"
 append "$loadrc" "$HOME/.profile"
--- update.sh
+++ update.sh
@@ -6,12 +6,12 @@ copyIfExists() {
     [ -f "$1" ] && ${SUDO_USER:+sudo -u "$SUDO_USER" env "PATH=$PATH"} cp -f "$1" "$2"
 }
 
-copyIfExists "$BENCH/shellrc.d/initrc.sh" "$INIT_REPO/shared/config/initrc.sh"
-copyIfExists "$BENCH/.gitignore-global" "$INIT_REPO/shared/config/.gitignore-global"
+copyIfExists "$BENCH/shellrc.d/initrc.sh" "$INIT_REPO/shared/etc/initrc.sh"
+copyIfExists "$BENCH/.gitignore-global" "$INIT_REPO/shared/etc/.gitignore-global"
 
 updateVSCode() {
-    copyIfExists "$1/settings.json" "$INIT_REPO/shared/config/vscode-settings.json"
-    copyIfExists "$1/keybindings.json" "$INIT_REPO/shared/config/vscode-keybindings.json"
+    copyIfExists "$1/settings.json" "$INIT_REPO/shared/etc/vscode-settings.json"
+    copyIfExists "$1/keybindings.json" "$INIT_REPO/shared/etc/vscode-keybindings.json"
 }
 
 if printf '%s' "$OSTYPE" | grep -q darwin 2>/dev/null; then
