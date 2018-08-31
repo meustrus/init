d29f1044cfc37050635eb8fb393f58f53620baea
meustrus <meustrus@gmail.com>
2018-08-31 12:53:38 -0500

    Implement `INIT_SSHCOPY=GITHUB` on Windows

---
+++ win/cli/ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+source "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
+
+source "$INIT_REPO/shared/cli/ssh.sh"
+
+if printf '%s' "$INIT_SSHCOPY" | grep -q GITHUB 2>/dev/null; then
+    clip < "$HOME/.ssh/id_ed25519.pub"
+    printf "\n\nCopied public key to clipboard.\nAdd public key to GitHub now.\n"
+    cmd //c start "https://github.com/settings/keys"
+    read -p "Press any key to continue..." -n 1 -s
+    printf "\n\n"
+}
