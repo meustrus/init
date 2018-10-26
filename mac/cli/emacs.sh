2294b7f5c44891d29a4ccdad1c24fd579ce24ce1
meustrus <meustrus@gmail.com>
2018-10-25 22:10:42 -0500

    Implement brew-linkapp for Emacs

--- mac/cli/emacs.sh
+++ mac/cli/emacs.sh
@@ -4,6 +4,6 @@
 
 brew tap d12frosted/emacs-plus
 brew-install emacs-plus --HEAD --with-natural-title-bars
-brew linkapps emacs-plus
+brew-linkapp /usr/local/opt/emacs-plus/Emacs.app
 
 . source-shared "cli/emacs.sh"
