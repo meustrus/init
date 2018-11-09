8131ca3041a0ce46c29df8a9300b2e69cbfae48f
meustrus <meustrus@gmail.com>
2018-11-09 10:04:08 -0600

    Improve deb/gui/vscode.sh

--- deb/gui/vscode.sh
+++ deb/gui/vscode.sh
@@ -5,6 +5,7 @@
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'printf "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main\n" > /etc/apt/sources.list.d/vscode.list'
 
+sudo apt update
 apt-install libgl1-mesa-glx libasound2 libgtk2.0-0 libxss1 code
 
 APPDATA="$HOME/.config" . source-shared "gui/vscode.sh"
