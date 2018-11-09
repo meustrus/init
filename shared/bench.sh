674bf4f6f22fa351b728c3980092e0417a23ed31
meustrus <meustrus@gmail.com>
2018-11-09 09:58:55 -0600

    Create $BENCH/apps folder

--- shared/bench.sh
+++ shared/bench.sh
@@ -3,5 +3,5 @@
 . "${INIT_REPO:-$(dirname "$0")/..}/vars.sh"
 
 install-var BENCH "$BENCH"
-mkdir -p "$BENCH/bin" "$BENCH/ext" "$BENCH/lib" "$BENCH/repo" "$BENCH/usr"
+mkdir -p "$BENCH/apps" "$BENCH/bin" "$BENCH/ext" "$BENCH/lib" "$BENCH/repo" "$BENCH/usr"
 install-path "$BENCH/bin"
--- win/choco.bat
+++ win/choco.bat
@@ -6,6 +6,13 @@ IF NOT DEFINED INIT_REPO (
     POPD
 )
 
+IF NOT DEFINED BENCH set BENCH="%USERPROFILE%\bench"
+md "%BENCH%" 2>nul
+md "%BENCH%\apps" 2>nul
+
+setx ChocolateyToolsLocation "%BENCH%\apps"
+set ChocolateyToolsLocation="%BENCH%\apps"
+
 choco -v >nul 2>&1 || (
     @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
     call "%INIT_REPO%\win\bin\refreshenv.bat"
