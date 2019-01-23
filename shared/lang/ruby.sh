c031da663442e669ebed81d98c2b62f4f0e94484
meustrus <meustrus@gmail.com>
2019-01-23 13:30:40 -0600

    Fix ruby install

--- shared/lang/ruby.sh
+++ shared/lang/ruby.sh
@@ -4,7 +4,7 @@
 
 INIT_LANG_RBENV=ON . source-impl "lang/rbenv.sh"
 
-RUBY_VERSION=`rbenv install -l | grep -v - | sort -V | tail -1`
+RUBY_VERSION=`rbenv install -l | grep -v - | sort -V | tail -1 | tr -d '[:space:]'`
 rbenv install "$RUBY_VERSION"
 rbenv global "$RUBY_VERSION"
 rbenv rehash
