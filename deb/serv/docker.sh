64d1cc62c86e220113df67f3ac62923228f940f7
meustrus <meustrus@gmail.com>
2019-01-23 15:09:53 -0600

    Add docker script for deb

---
+++ deb/serv/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if ! test -f /etc/apt/sources.list.d/docker.list; then
+    apt-install apt-transport-https ca-certificates gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo sh -c 'printf "deb [arch=amd64] https://download.docker.com/linux/debian `lsb_release -cs` stable\n" > /etc/apt/sources.list.d/docker.list'
+    sudo apt update
+fi
+
+apt-install docker-ce
+
+sudo usermod -aG docker "$USER"
