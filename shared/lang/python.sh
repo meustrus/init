793c18fb41993cdcec2544400263e426ddd5a68b
meustrus <meustrus@gmail.com>
2019-01-23 13:46:30 -0600

    Stop telling pip to install pip

--- shared/lang/python.sh
+++ shared/lang/python.sh
@@ -3,7 +3,7 @@
 . "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
 if [ -x "$(command -v pip)" ]; then
-    pip install --quiet --upgrade pip setuptools
+    pip install --quiet --upgrade setuptools
 else
     printf 'ERROR: Python not installed!\n' 1>&2
 fi
