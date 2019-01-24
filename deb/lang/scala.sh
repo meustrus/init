0e514b2f8c56751587c5edf74eb36655a90e4d16
meustrus <meustrus@gmail.com>
2019-01-24 15:48:37 -0600

    Implement dev/lang/scala.sh

--- deb/lang/scala.sh
+++ deb/lang/scala.sh
@@ -2,7 +2,11 @@
 
 . "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
-brew-install scala
-brew-install sbt
+apt-install dirmngr
+
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+apt-install sbt
 
 . source-shared "lang/scala.sh"
