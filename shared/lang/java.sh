cebbbacf25354ef28dc6c16b15bc8bfcb2c21109
meustrus <meustrus@gmail.com>
2019-01-25 13:16:18 -0600

    Remove bad maven environment variables

--- shared/lang/java.sh
+++ shared/lang/java.sh
@@ -2,12 +2,4 @@
 
 . "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
-if [ -x "$(command -v java)" ]; then
-    mkdir -p "$BENCH/lib/maven/bin"
-    install-var M2 "$BENCH/lib/maven"
-    install-var M2_HOME "$BENCH/lib/maven/bin"
-    install-var MAVEN_OPTS "-Xms1024m -Xmx2048m"
-    install-path "$BENCH/lib/maven/bin"
-else
-    printf 'ERROR: Java not installed!\n' 1>&2
-fi
+install-var MAVEN_OPTS "-Xms1024m -Xmx2048m"
