90a281de07c07c5bd9b24144c6043cc99505b78d
meustrus <meustrus@gmail.com>
2019-01-30 13:42:01 -0600

    Install pipenv with python

--- shared/lang/python.sh
+++ shared/lang/python.sh
@@ -3,7 +3,7 @@
 . "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
 if [ -x "$(command -v pip)" ]; then
-    pip install --quiet --upgrade setuptools
+    pip install --quiet --upgrade setuptools pipenv
 else
     printf 'ERROR: Python not installed!\n' 1>&2
 fi
