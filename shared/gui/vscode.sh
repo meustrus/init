604624c2cbbaec5dbb69944016b551ac5be2e31e
meustrus <meustrus@gmail.com>
2019-03-19 12:07:12 -0500

    Improve VS Code settings with better extensions

--- shared/gui/vscode.sh
+++ shared/gui/vscode.sh
@@ -15,24 +15,25 @@ if [ -x "$(command -v code)" ] && [ -d "$APPDATA" ]; then
 
     yes 0 | code \
         --install-extension    formulahendry.auto-close-tag \
-        --install-extension         stayfool.vscode-asciidoc \
         --install-extension        jetmartin.bats \
         --install-extension       aaron-bond.better-comments \
+        --install-extension        marlon407.code-groovy \
         --install-extension        ms-vscode.csharp \
+        --install-extension      alexkrechik.cucumberautocomplete \
         --install-extension     editorconfig.editorconfig \
-        --install-extension          eamodio.gitlens \
         --install-extension         eriklynd.json-tools \
         --install-extension christian-kohler.npm-intellisense \
-        --install-extension          tootone.org-mode \
         --install-extension        ms-python.python \
         --install-extension             2gua.rainbow-brackets \
         --install-extension      mechatroner.rainbow-csv \
         --install-extension         rebornix.ruby \
         --install-extension      itryapitsin.sbt \
-        --install-extension           dragos.scala-lsp \
+        --install-extension        scalameta.metals \
         --install-extension      craigthomas.supersharp \
+        --install-extension         stayfool.vscode-asciidoc \
         --install-extension  robertohuertasm.vscode-icons \
         --install-extension          vscjava.vscode-java-pack \
+        --install-extension        ms-vscode.wordcount \
         --install-extension   DotJoshJohnson.xml \
         ;
 elif [ -x "$(command -v code)" ]; then
