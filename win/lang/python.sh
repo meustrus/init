4b1fe189e34c1ccf583d74939f236c20e7765faf
meustrus <meustrus@gmail.com>
2019-06-14 15:48:47 -0500

    Fix Python install on Windows failing due to "invalid characters in path"

--- win/lang/python.sh
+++ win/lang/python.sh
@@ -2,6 +2,6 @@
 
 . "${INIT_REPO:-$(dirname "$0")/../..}/vars.sh"
 
-choco install python2 -y --package-parameters="/InstallDir:$BENCH/lib/python2"
+choco install python2 -y --package-parameters="/InstallDir:$BENCH\\lib\\python2"
 
 . source-shared "lang/python.sh"
