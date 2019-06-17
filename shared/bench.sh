e97bc98a476dcfecd69ed351eb3922eb72155a04
meustrus <meustrus@gmail.com>
2019-06-17 15:26:29 -0500

    Fix install of $BENCH variable failing on first run

--- shared/bench.sh
+++ shared/bench.sh
@@ -2,6 +2,6 @@
 
 . "${INIT_REPO:-$(dirname "$0")/..}/vars.sh"
 
-install-var BENCH "$BENCH"
 mkdir -p "$BENCH/apps" "$BENCH/bin" "$BENCH/ext" "$BENCH/lib" "$BENCH/repo" "$BENCH/usr"
+install-var BENCH "$BENCH"
 install-path "$BENCH/bin"
