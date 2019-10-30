a28f4473dab990b78e4aff8c0553b819483a396a
meustrus <meustrus@gmail.com>
2019-10-30 16:40:18 -0500

    Update VS Code settings

--- shared/gui/vscode.sh
+++ shared/gui/vscode.sh
@@ -15,7 +15,6 @@ if [ -x "$(command -v code)" ] && [ -d "$APPDATA" ]; then
 
     yes 0 | code \
         --install-extension            ryu1kn.annotator \
-        --install-extension      adamhartford.vscode-base64 \
         --install-extension         jetmartin.bats \
         --install-extension        aaron-bond.better-comments \
         --install-extension         marlon407.code-groovy \
@@ -24,20 +23,25 @@ if [ -x "$(command -v code)" ] && [ -d "$APPDATA" ]; then
         --install-extension      editorconfig.editorconfig \
         --install-extension            sohibe.java-generate-setters-getters \
         --install-extension          eriklynd.json-tools \
+        --install-extension            yzhang.markdown-all-in-one \
+        --install-extension         scalameta.metals \
         --install-extension  christian-kohler.npm-intellisense \
+        --install-extension         adammaras.overtype \
         --install-extension         ms-python.python \
         --install-extension              2gua.rainbow-brackets \
         --install-extension       mechatroner.rainbow-csv \
+        --install-extension             humao.rest-client \
         --install-extension          rebornix.ruby \
         --install-extension       itryapitsin.sbt \
-        --install-extension            yzhang.markdown-all-in-one \
-        --install-extension         scalameta.metals \
         --install-extension              mtxr.sqltools \
         --install-extension       craigthomas.supersharp \
+        --install-extension              jock.svg \
         --install-extension             mauve.terraform \
         --install-extension           flesler.url-encode \
         --install-extension          karb0f0s.vbscript \
         --install-extension          stayfool.vscode-asciidoc \
+        --install-extension      adamhartford.vscode-base64 \
+        --install-extension           aeschli.vscode-css-formatter \
         --install-extension         slevesque.vscode-hexdump \
         --install-extension vscode-icons-team.vscode-icons \
         --install-extension           vscjava.vscode-java-pack \
