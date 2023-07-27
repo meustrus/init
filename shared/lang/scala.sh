0e4b798d28920f2b5940535672fbe6223e6e5b91
sjenna <jtschwartz@jackhenry.com>
2023-07-27 17:35:27 -0500

    Make Scala install work

--- shared/lang/scala.sh
+++ shared/lang/scala.sh
@@ -6,10 +6,14 @@ if [ -x "$(command -v sbt)" ]; then
     # mkdir -p "$BENCH/lib/ivy2" "$BENCH/lib/sbt"
     # . install-var SBT_OPTS "-Xmx3g -Xms512M -XX:MaxMetaspaceSize=2048M -XX:-UseGCOverheadLimit -Dsbt.ivy.home=\"$BENCH/lib/ivy2/\" -Divy.home=\"$BENCH/lib/ivy2/\" -Dsbt.boot.directory=\"$BENCH/lib/sbt/\""
 
+    mkdir -p "$HOME/.sbt"
+    mkdir -p "$HOME/.sbt/0.13"
+    mkdir -p "$HOME/.sbt/1.0"
+
     touch "$HOME/.sbt/.credentials"
     touch "$HOME/.sbt/.credentials-coursier"
-    echo 'credentials += Credentials(Path.userHome / ".sbt" / ".credentials")' >> "$HOME/.sbt/0.13/global.sbt"
-    echo 'credentials += Credentials(Path.userHome / ".sbt" / ".credentials")' >> "$HOME/.sbt/1.0/global.sbt"
+    append 'credentials += Credentials(Path.userHome / ".sbt" / ".credentials")' "$HOME/.sbt/0.13/global.sbt"
+    append 'credentials += Credentials(Path.userHome / ".sbt" / ".credentials")' "$HOME/.sbt/1.0/global.sbt"
 
     . install-var COURSIER_CREDENTIALS "$HOME/.sbt/.credentials-coursier"
     . install-var SBT_CREDENTIALS "$HOME/.sbt/.credentials"
